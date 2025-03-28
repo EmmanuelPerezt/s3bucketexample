5559c3a01f79e67fc180b5ceab48ee68df3e00d4
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-03-27 19:14:05 -0600

    fix: detalles menores

--- src/controller/uploadImage.ts
+++ src/controller/uploadImage.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import dotenv from 'dotenv'
-import {s3,} from '../config/config'
+import {s3} from '../config/config'
 dotenv.config()
 
 
@@ -8,17 +8,16 @@ export const uploadImage:any = (req:Request,res:Response) => {
     if(!req.file){
         res.status(400).send('envie un archivo')
     }
+    //afinar detalles de los params.
     const params = {
         Bucket: process.env.aws_bucket_name ||'',
         Body: req.file?.originalname || '',
         Key: req.file?.filename || ''
     }
-    console.log(req.file)
     s3.upload(params, (err:any, data:any) => {
         if(err){
             res.status(400).send({message: err})
         }
         res.status(200).send({'enviado con exito': {data}})
     })
-    
 }
